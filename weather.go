02346f46b5627cb66b90ae3f23656b0ff46b9ca1
Daniele Sluijters <daenney@users.noreply.github.com>
2022-07-09 19:52:30 +0200

    Docs: Add comments for WeatherStation

--- weather.go
+++ weather.go
@@ -2,38 +2,74 @@ package trafikinfo
 
 import "time"
 
+// WeatherStation1Dot0 is the WeatherStation v1.0 model. It returns all kinds
+// of environmental data
 type WeatherStation1Dot0 struct {
-	Active      *bool     `json:"Active,omitempty"`
-	County      []County  `json:"CountyNo,omitempty"`
-	Deleted     *bool     `json:"Deleted,omitempty"`
-	Geometry    *Geometry `json:"Geometry,omitempty"`
-	IconID      *string   `json:"IconId,omitempty"`
-	ID          *string   `json:"Id,omitempty"`
+	// Active indicates this station is returning data
+	Active *bool `json:"Active,omitempty"`
+	// County is the Swedish county in which this station is located
+	County []County `json:"CountyNo,omitempty"`
+	// Deleted indicates if this is a deleted object. This should only
+	// ever be true if you created a Query with IncludeDeletedItems
+	Deleted *bool `json:"Deleted,omitempty"`
+	// Geometry contains coordinates for where this station is located
+	Geometry *Geometry `json:"Geometry,omitempty"`
+	IconID   *string   `json:"IconId,omitempty"`
+	// ID is the unique ID identifying this station
+	ID *string `json:"Id,omitempty"`
+	// Measurement represents measurement data from environmental sensors
 	Measurement *struct {
+		// Air contains the typical things like relative humidity and temperature
+		// as measured in the air
 		Air *struct {
-			RelativeHumidity  *float64 `json:"RelativeHumidity,omitempty"`
+			// RelativeHumidity is the relative humidity in percent
+			RelativeHumidity *float64 `json:"RelativeHumidity,omitempty"`
+			// Temperature is measured in degrees Celsius
 			Temperature       *float64 `json:"Temp,omitempty"`
 			TemperatureIconID *string  `json:"TempIconId,omitempty"`
 		} `json:"Air,omitempty"`
-		MeasureTime   *time.Time `json:"MeasureTime,omitempty"`
+		// MeasurTime indicates when these values where recorded
+		MeasureTime *time.Time `json:"MeasureTime,omitempty"`
+		// Precipitation contains the type and amount of precipitation
 		Precipitation *struct {
-			Amount     *float64             `json:"Amount,omitempty"`
+			// Amount is the mount of rain in mm per hour. It's worth noting
+			// that in practice it turns out the absence of a value indicates
+			// "no rain", i.e precipitation of 0, not "this station doesn't
+			// report precipitation", unless Measurement was Excluded by the
+			// Query
+			Amount *float64 `json:"Amount,omitempty"`
+			// AmountName is a predefined string describing the type and amount
+			// of precipitation in Swedish
 			AmountName *PrecipitationAmount `json:"AmountName,omitempty"`
-			Type       *PrecipitationType   `json:"Type,omitempty"`
-			TypeIconID *string              `json:"TypeIconId,omitempty"`
+			// Type is a predefined string describing the type of precipitation
+			// in Swedish
+			Type       *PrecipitationType `json:"Type,omitempty"`
+			TypeIconID *string            `json:"TypeIconId,omitempty"`
 		} `json:"Precipitation,omitempty"`
+		// Road contains the temperature as measured at the road deck level.
+		// This can be much higher or lower than the Air values
 		Road *struct {
+			// Temperature is measured in degrees Celsius
 			Temperature       *float64 `json:"Temp,omitempty"`
 			TemperatureIconID *string  `json:"TempIconId,omitempty"`
 		} `json:"Road,omitempty"`
+		// Wind contains wind direction and strength
 		Wind *struct {
-			Direction       *float64       `json:"Direction,omitempty"`
-			DirectionIconID *string        `json:"DirectionIconId,omitempty"`
-			DirectionText   *WindDirection `json:"DirectionText,omitempty"`
-			Force           *float64       `json:"Force,omitempty"`
-			ForceMax        *float64       `json:"ForceMax,omitempty"`
+			// Direction is represented in degrees
+			Direction       *float64 `json:"Direction,omitempty"`
+			DirectionIconID *string  `json:"DirectionIconId,omitempty"`
+			// DirectionText is a predefined string representing the wind
+			// direction in Swedish
+			DirectionText *WindDirection `json:"DirectionText,omitempty"`
+			// Force is the 10 minute average in m/sec
+			Force *float64 `json:"Force,omitempty"`
+			// ForceMax is the 30 minute max in m/sec
+			ForceMax *float64 `json:"ForceMax,omitempty"`
 		} `json:"Wind,omitempty"`
 	} `json:"Measurement,omitempty"`
+	// MeasurementHistory contains multiple instances of Measurement
+	// over time. See the comments on Measurement for how to interpret
+	// the data
 	MeasurementHistory []struct {
 		Air *struct {
 			RelativeHumidity  *float64 `json:"RelativeHumidity,omitempty"`
@@ -59,9 +95,12 @@ type WeatherStation1Dot0 struct {
 			ForceMax        *float64       `json:"ForceMax,omitempty"`
 		} `json:"Wind,omitempty"`
 	} `json:"MeasurementHistory,omitempty"`
+	// ModifiedTime represents when this object was last modified
 	ModifiedTime *time.Time `json:"ModifiedTime,omitempty"`
-	Name         *string    `json:"Name,omitempty"`
-	RoadNumber   *int       `json:"RoadNumberNumeric,omitempty"`
+	// Name is the name of the station
+	Name *string `json:"Name,omitempty"`
+	// RoadNumber is the Swedish road number this station is located at
+	RoadNumber *int `json:"RoadNumberNumeric,omitempty"`
 }
 
 type PrecipitationAmount string
